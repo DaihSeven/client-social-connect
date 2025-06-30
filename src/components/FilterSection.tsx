788c620a4da12fa900fdc7762b678693c2585a2f
Daiane <daihbarbosa16@gmail.com>
2025-06-30 11:56:32 -0300

    fix: :bug: :ok_hand: fixed routes for use in filters

---
+++ src/components/FilterSection.tsx
@@ -0,0 +1,83 @@
+import { ChangeEvent } from 'react';
+
+type FilterOption = {
+  label: string;
+  value: string;
+};
+
+type Filters = {
+  city: string;
+  type: string;
+  localization: string;
+};
+
+type FilterSectionProps = {
+  readonly cityOptions: FilterOption[];
+  readonly typeOptions: FilterOption[];
+  readonly localizationOptions: FilterOption[];
+  readonly filters: Filters;
+  readonly onFilterChange: (updateFilters: Filters) => void;
+};
+
+export default function FilterSection({
+  cityOptions,
+  typeOptions,
+  localizationOptions,
+  filters,
+  onFilterChange,
+}: FilterSectionProps) {
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    onFilterChange({
+      ...filters,
+      [name as keyof Filters]: value,
+    });
+  };
+
+  const handleReset = () => {
+  onFilterChange({
+    city: '',
+    type: '',
+    localization: '',
+  });
+};
+
+  return (
+    <section className="flex flex-col gap-4 md:flex-row md:items-center">
+      <select name="city" title="Filtro por cidade" value={filters.city ?? ''} onChange={handleChange} className="p-2 border rounded">
+        <option value="">Selecione uma cidade</option>
+        {cityOptions.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+
+      <select name="type" title="Filtro por tipo" value={filters.type ?? ''} onChange={handleChange} className="p-2 border rounded">
+        <option value="">Selecione um tipo</option>
+        {typeOptions.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+
+      <select name="localization" title="Filtro por localização" value={filters.localization ?? ''} onChange={handleChange} className="p-2 border rounded">
+        <option value="">Todas as localizações</option>
+        {localizationOptions.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+
+      <button
+        type="button"
+        onClick={handleReset}
+        className="p-2 border border-red-500 text-red-500 rounded hover:bg-red-100 transition"
+      >
+        Limpar filtros
+      </button>
+    </section>
+  );
+}
